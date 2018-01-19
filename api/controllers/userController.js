5ddf88ef63e15b0737141dad8bdadef47d40ed41
sinsil <sinsilmathew@yahoo.com>
2018-01-19 16:51:41 +0530

    BCRYPT

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,8 +2,12 @@ const mongoose=require('mongoose');
 //const User = require('../models/userModel');
 const User = mongoose.model('User');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const addUser = function(req,res){
+    req.body.Password = bcrypt.hashSync(req.body.Password,8);
+    const decrypt = bcrypt.compareSync("Pass1",req.body.password);
+    console.log(decrypt);
     const newUser=new User(req.body);
     newUser.save(function(err){
         if(err){res.json({'ERROR':err})}
